0308dec378924dfb3dfd5eee38230f5c011c27e5
luckyryn30 <132465471+luckyryn30@users.noreply.github.com>
2023-06-20 17:43:21 +0530

    Add files via upload

---
+++ game_with_board.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<cmath>
+using namespace std;
+using ll=long long int;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+     int n;
+     cin>>n;
+     if(n>=5)
+     {
+        cout<<"Alice"<<endl;
+     }
+     else
+     {
+        cout<<"Bob"<<endl;
+     }
+    }
+    return 0;
+}
